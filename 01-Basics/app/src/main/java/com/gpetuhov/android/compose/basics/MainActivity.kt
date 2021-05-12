4c6744de4ae0e2851f9697c15eb4412527809981
Gennady Petukhov <g3pmail@gmail.com>
2021-05-12 14:14:48 +0300

    Add Modifier

--- 01-Basics/app/src/main/java/com/gpetuhov/android/compose/basics/MainActivity.kt
+++ 01-Basics/app/src/main/java/com/gpetuhov/android/compose/basics/MainActivity.kt
@@ -3,12 +3,17 @@ package com.gpetuhov.android.compose.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.basics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainActivityUI() {
-    Button(onClick = { onButtonClick() }) {
+    Button(
+        onClick = { onButtonClick() },
+        modifier = Modifier
+            .width(200.dp)
+            .height(48.dp)
+        // This can also be done as
+//        modifier = Modifier
+//            .size(width = 200.dp, height = 48.dp)
+
+    ) {
         Text(text = "Press me")
     }
 }
