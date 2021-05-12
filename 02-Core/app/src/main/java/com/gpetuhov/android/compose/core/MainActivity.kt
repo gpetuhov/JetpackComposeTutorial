79fcf303aae2276e0bd698df69ba973bc85b67a1
Gennady Petukhov <g3pmail@gmail.com>
2021-05-12 16:35:17 +0300

    Add Surface

--- 02-Core/app/src/main/java/com/gpetuhov/android/compose/core/MainActivity.kt
+++ 02-Core/app/src/main/java/com/gpetuhov/android/compose/core/MainActivity.kt
@@ -3,10 +3,13 @@ package com.gpetuhov.android.compose.core
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.gpetuhov.android.compose.core.ui.theme.JetpackComposeCoreTheme
 
@@ -14,25 +17,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeCoreTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    // Surface is a portion of the screen with color
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeCoreTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
