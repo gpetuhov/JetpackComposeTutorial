768c499798594b05e4b2f476e13e5b6339e660ee
Gennady Petukhov <g3pmail@gmail.com>
2021-05-12 19:16:27 +0300

    Add Row

--- 02-Core/app/src/main/java/com/gpetuhov/android/compose/core/MainActivity.kt
+++ 02-Core/app/src/main/java/com/gpetuhov/android/compose/core/MainActivity.kt
@@ -3,8 +3,7 @@ package com.gpetuhov.android.compose.core
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,19 +11,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            MainScreen2()
         }
     }
 }
 
 @Composable
 fun MainScreen() {
-    // Surface is a portion of the screen with color
+    // Surface is a portion of the screen with color.
+    // Surface can have only one child.
     Surface(
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
@@ -42,8 +43,33 @@ fun MainScreen() {
     }
 }
 
+@Composable
+fun MainScreen2() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Row can have many children arranged horizontally
+        Row {
+            Surface(
+                color = Color.Yellow,
+                modifier = Modifier
+                    .height(300.dp)
+                    .width(60.dp)
+            ) { }
+
+            Surface(
+                color = Color.Green,
+                modifier = Modifier
+                    .height(300.dp)
+                    .width(60.dp)
+            ) { }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainScreen()
+    MainScreen2()
 }
