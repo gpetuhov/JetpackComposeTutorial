0fda1db7b1d6ad87ace1e219b85e309bab0033ea
Gennady Petukhov <g3pmail@gmail.com>
2021-05-13 12:46:26 +0300

    Add names list

--- 03-Dynamic/app/src/main/java/com/gpetuhov/android/compose/dynamic/MainActivity.kt
+++ 03-Dynamic/app/src/main/java/com/gpetuhov/android/compose/dynamic/MainActivity.kt
@@ -3,36 +3,38 @@ package com.gpetuhov.android.compose.dynamic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.gpetuhov.android.compose.dynamic.ui.theme.JetpackComposeDynamicContentTheme
+
+val namesList = listOf("John", "Bill", "Kate")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeDynamicContentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    NamesList(names = namesList)
+}
+
+@Composable
+fun NamesList(names: List<String>) {
+    Column {
+        names.forEach {
+            Text(text = it)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeDynamicContentTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
