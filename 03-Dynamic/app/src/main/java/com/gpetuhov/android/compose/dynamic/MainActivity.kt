ccc2fc7ffb84bf9f0e5dfc6c6da53d44c1ac7e56
Gennady Petukhov <g3pmail@gmail.com>
2021-05-13 13:47:39 +0300

    Add state

--- 03-Dynamic/app/src/main/java/com/gpetuhov/android/compose/dynamic/MainActivity.kt
+++ 03-Dynamic/app/src/main/java/com/gpetuhov/android/compose/dynamic/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 
 val namesList = mutableListOf(
@@ -19,7 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            MainScreen3()
         }
     }
 }
@@ -37,7 +39,56 @@ fun NamesList() {
         Text(text = it)
     }
 
+    // This will NOT update screen,
+    // because here namesList is NOT the state of our Composable
     Button(onClick = { namesList.add("New name") }) {
+        Text(text = "Add new name")
+    }
+}
+
+@Composable
+fun MainScreen2() {
+    Column {
+        NamesListWithState()
+    }
+}
+
+@Composable
+fun NamesListWithState() {
+    // Here a Composable holds and controls its own state, and this is not good.
+    // We have to lift up the state (aka state hoisting) - see MainScreen3
+    val namesListState = remember { mutableStateListOf("John", "Bill", "Kate") }
+
+    namesListState.forEach {
+        Text(text = it)
+    }
+
+    // Here the state is updated, and this will trigger screen update
+    // (screen updte is called Recomposition)
+    Button(onClick = { namesListState.add("New name") }) {
+        Text(text = "Add new name")
+    }
+}
+
+@Composable
+fun MainScreen3() {
+    val namesListState = remember { mutableStateListOf("John", "Bill", "Kate") }
+
+    Column {
+        NamesList(namesListState) { namesListState.add("New name") }
+    }
+}
+
+@Composable
+fun NamesList(
+    names: List<String>,
+    onButtonClick: () -> Unit
+) {
+    names.forEach {
+        Text(text = it)
+    }
+
+    Button(onClick = onButtonClick) {
         Text(text = "Add new name")
     }
 }
@@ -45,5 +96,5 @@ fun NamesList() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainScreen()
+    MainScreen3()
 }
