c10ac2b440cbe82db41fa5be972c64ae43c84533
Gennady Petukhov <g3pmail@gmail.com>
2021-05-13 16:00:56 +0300

    Add card

--- 04-Complex/app/src/main/java/com/gpetuhov/android/compose/complexlayout/MainActivity.kt
+++ 04-Complex/app/src/main/java/com/gpetuhov/android/compose/complexlayout/MainActivity.kt
@@ -3,36 +3,55 @@ package com.gpetuhov.android.compose.complexlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.complexlayout.ui.theme.JetpackComposeComplexLayoutTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeComplexLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.LightGray
+    ) {
+        ProfileCard()
+    }
+}
+
+@Composable
+fun ProfileCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        backgroundColor = Color.Red,
+        elevation = 8.dp
+    ) {
+        Text(text = "Hello, world!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeComplexLayoutTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
