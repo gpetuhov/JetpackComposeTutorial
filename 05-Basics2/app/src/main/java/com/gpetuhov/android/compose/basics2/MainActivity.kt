7c51aab5488fce5ab18fdc338303ec46f77bbd07
Gennady Petukhov <g3pmail@gmail.com>
2021-05-15 17:29:09 +0300

    Change text style

--- 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
+++ 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.basics2.ui.theme.JetpackComposeBasics2Theme
@@ -39,6 +41,7 @@ fun NewsStory() {
             modifier = Modifier
                 .height(180.dp)
                 .fillMaxWidth()
+                // This adds round corners to the image
                 .clip(shape = RoundedCornerShape(4.dp)),
             contentScale = ContentScale.Crop
         )
@@ -46,9 +49,25 @@ fun NewsStory() {
         // Separates widgets
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+        Text(
+            text = "A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+            style = typography.h6,
+            // This limits the text to 2 lines and adds ellipsis if needed
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Text(
+            text = "Davenport, California",
+            style = typography.body2
+        )
+
+        Text(
+            text = "December 2018",
+            style = typography.body2
+        )
     }
 }
 
