17261a4bd5c58df33d5dc36bb97efc65a4d51fdf
Gennady Petukhov <g3pmail@gmail.com>
2021-05-15 17:19:18 +0300

    Change image style

--- 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
+++ 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,8 +32,15 @@ fun NewsStory() {
     ) {
         Image(
             painter = painterResource(R.drawable.header),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
         )
+
+        // Separates widgets
+        Spacer(modifier = Modifier.height(16.dp))
 
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
