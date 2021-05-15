2836a4b3d8c0672532fd0274305ba5ad9dd80b4b
Gennady Petukhov <g3pmail@gmail.com>
2021-05-15 16:58:06 +0300

    Add text

--- 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
+++ 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
@@ -3,36 +3,39 @@ package com.gpetuhov.android.compose.basics2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.basics2.ui.theme.JetpackComposeBasics2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasics2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasics2Theme {
-        Greeting("Android")
-    }
+    NewsStory()
 }
