bf375fcb1722f55a5b3dba1472c3410d4a1f8b54
Gennady Petukhov <g3pmail@gmail.com>
2021-05-15 17:12:33 +0300

    Add image

--- 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
+++ 05-Basics2/app/src/main/java/com/gpetuhov/android/compose/basics2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gpetuhov.android.compose.basics2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.basics2.ui.theme.JetpackComposeBasics2Theme
@@ -28,6 +30,11 @@ fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null
+        )
+
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
