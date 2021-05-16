066cf86df06a0eec52aa96c22020ea1d5bd1dde8
Gennady Petukhov <g3pmail@gmail.com>
2021-05-16 19:09:40 +0300

    Change text style

--- 06-BasicsCodelab/app/src/main/java/com/gpetuhov/android/compose/basicscodelab/MainActivity.kt
+++ 06-BasicsCodelab/app/src/main/java/com/gpetuhov/android/compose/basicscodelab/MainActivity.kt
@@ -3,11 +3,15 @@ package com.gpetuhov.android.compose.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.basicscodelab.ui.theme.JetpackComposeBasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +30,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicsCodelabTheme {
