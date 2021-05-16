2570b81b659b4504b0d0086c3e45f4fa3d4b0ee4
Gennady Petukhov <g3pmail@gmail.com>
2021-05-16 20:05:08 +0300

    Add button with state

--- 06-BasicsCodelab/app/src/main/java/com/gpetuhov/android/compose/basicscodelab/MainActivity.kt
+++ 06-BasicsCodelab/app/src/main/java/com/gpetuhov/android/compose/basicscodelab/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,20 +41,32 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent() {
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
     Column {
-        Greeting("Android")
+        for (name in names) {
+            Greeting(name = name)
 
-        // Creates horizontal divider
-        Divider(color = Color.Black)
-
-        Greeting("there")
+            // Creates horizontal divider
+            Divider(color = Color.Black)
+        }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter()
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter() {
+
+    val count = remember { mutableStateOf(0) }
+
+    Button(onClick = { count.value++ }) {
+        Text("I've been clicked ${count.value} times")
+    }
 }
 
 @Preview
