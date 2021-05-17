383dfcd211dd93741bd04f4cbd5d53f36203bfe9
Gennady Petukhov <g3pmail@gmail.com>
2021-05-17 16:55:28 +0300

    Add photographer card

--- 07-LayoutsCodelab/app/src/main/java/com/gpetuhov/android/compose/layouts/MainActivity.kt
+++ 07-LayoutsCodelab/app/src/main/java/com/gpetuhov/android/compose/layouts/MainActivity.kt
@@ -3,11 +3,18 @@ package com.gpetuhov.android.compose.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.layouts.ui.theme.JetpackComposeLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +33,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+    )
+}
+
+@Composable
+fun PhotographerCard() {
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun PhotographerCardPreview() {
+    JetpackComposeLayoutsTheme {
+        PhotographerCard()
+    }
 }
 
 @Preview(showBackground = true)
