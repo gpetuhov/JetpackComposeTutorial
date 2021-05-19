64125b130cdd6753358c26c3c52835ee817e5836
Gennady Petukhov <g3pmail@gmail.com>
2021-05-19 14:33:02 +0300

    Add ConstraintSet

--- 10-ConstraintLayout/app/src/main/java/com/gpetuhov/android/compose/constraintlayout/MainActivity.kt
+++ 10-ConstraintLayout/app/src/main/java/com/gpetuhov/android/compose/constraintlayout/MainActivity.kt
@@ -3,15 +3,19 @@ package com.gpetuhov.android.compose.constraintlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.compose.atLeast
 import com.gpetuhov.android.compose.constraintlayout.ui.theme.JetpackComposeConstraintLayoutTheme
@@ -23,7 +27,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeConstraintLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LargeConstraintLayout()
+                    DecoupledConstraintLayout()
                 }
             }
         }
@@ -110,10 +114,48 @@ fun LargeConstraintLayout() {
     }
 }
 
+// Decoupled API
+// We can create a separate ConstraintSet and apply it to ConstraintLayout
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
+            }
+
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin= margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ConstraintLayoutContentPreview() {
     JetpackComposeConstraintLayoutTheme {
-        LargeConstraintLayout()
+        DecoupledConstraintLayout()
     }
 }
