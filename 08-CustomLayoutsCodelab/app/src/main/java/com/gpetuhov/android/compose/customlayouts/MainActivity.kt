07da1b334cf5011af8f76db841fb72217ee6b467
Gennady Petukhov <g3pmail@gmail.com>
2021-05-19 11:25:09 +0300

    Add custom layout

--- 08-CustomLayoutsCodelab/app/src/main/java/com/gpetuhov/android/compose/customlayouts/MainActivity.kt
+++ 08-CustomLayoutsCodelab/app/src/main/java/com/gpetuhov/android/compose/customlayouts/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeCustomLayoutsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BodyContent()
                 }
             }
         }
@@ -37,6 +38,8 @@ class MainActivity : ComponentActivity() {
 fun Modifier.firstBaselineToTop(
     firstBaselineToTop: Dp
 ) = this.layout { measurable, constraints ->
+    // The result of the measure function is placeable,
+    // which can be placed inside the parent.
     val placeable = measurable.measure(constraints)
 
     // Check the composable has a first baseline
@@ -68,15 +71,57 @@ fun TextWithNormalPaddingPreview() {
     }
 }
 
+// Here we create our own custom layout (we implement a basic Column).
+// This example show how to change the layout of many composables
+// (of all the composables, that are inside the content of our custom layout).
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // In this example we don't constrain child views further,
+        // measure them with given constraints.
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeCustomLayoutsTheme {
-        Greeting("Android")
+        BodyContent()
     }
 }
