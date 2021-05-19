765f843faa807ab8cceb9b01f1e48faca4ce189c
Gennady Petukhov <g3pmail@gmail.com>
2021-05-19 15:35:41 +0300

    Add intrinsic height

--- 11-Intrinsics/app/src/main/java/com/gpetuhov/android/compose/intrinsics/MainActivity.kt
+++ 11-Intrinsics/app/src/main/java/com/gpetuhov/android/compose/intrinsics/MainActivity.kt
@@ -3,11 +3,17 @@ package com.gpetuhov.android.compose.intrinsics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.intrinsics.ui.theme.JetpackComposeIntrinsicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +23,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeIntrinsicsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    TwoTexts(text1 = "Hi", text2 = "there")
                 }
             }
         }
@@ -25,14 +31,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    // height(IntrinsicSize.Min) sizes its children being forced
+    // to be as tall as their minimum intrinsic height.
+    // This will make the Divider to be as tall as the tallest Text.
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun TwoTextsPreview() {
     JetpackComposeIntrinsicsTheme {
-        Greeting("Android")
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
     }
 }
