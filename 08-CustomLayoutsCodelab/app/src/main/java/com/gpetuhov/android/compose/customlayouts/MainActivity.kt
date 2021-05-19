8f883b31a0c0fcdb89d25bb50decce45d0566bf5
Gennady Petukhov <g3pmail@gmail.com>
2021-05-19 10:49:47 +0300

    Add layout modifier

--- 08-CustomLayoutsCodelab/app/src/main/java/com/gpetuhov/android/compose/customlayouts/MainActivity.kt
+++ 08-CustomLayoutsCodelab/app/src/main/java/com/gpetuhov/android/compose/customlayouts/MainActivity.kt
@@ -3,11 +3,18 @@ package com.gpetuhov.android.compose.customlayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.gpetuhov.android.compose.customlayouts.ui.theme.JetpackComposeCustomLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +28,43 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+// Here we create a modifier, that adds padding to first baseline,
+// instead of the padding to top of the composable.
+// This example shows how to change the layout of only one composable.
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    JetpackComposeCustomLayoutsTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    JetpackComposeCustomLayoutsTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
