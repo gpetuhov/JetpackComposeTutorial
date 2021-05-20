ecc8cdccf67a18b108da108693061dac4acaeb72
Gennady Petukhov <g3pmail@gmail.com>
2021-05-20 08:09:29 +0300

    Show animated icon row

--- 12-StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ 12-StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -17,12 +17,7 @@
 package com.codelabs.state.todo
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -145,6 +140,9 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     // it's updated so compose can recompose any composables that read it.
     val (text, setText) = remember { mutableStateOf("") }
 
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default)}
+    val iconsVisible = text.isNotBlank()
+
     Column {
         Row(Modifier
             .padding(horizontal = 16.dp)
@@ -166,6 +164,12 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank() // enable if text is not blank
             )
+        }
+
+        if (iconsVisible) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
