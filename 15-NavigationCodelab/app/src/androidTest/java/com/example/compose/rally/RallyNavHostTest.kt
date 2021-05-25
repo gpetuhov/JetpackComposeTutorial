a78815164f2a81246e111313ad3de24b208e224e
Gennady Petukhov <g3pmail@gmail.com>
2021-05-25 06:55:21 +0300

    Check if overview screen is displayed

--- 15-NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ 15-NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -1,24 +1,29 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.junit.Rule
 import org.junit.Test
-import org.junit.Assert.fail
 
 class RallyNavHostTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+    lateinit var navController: NavHostController
 
     @Test
     fun rallyNavHost() {
         composeTestRule.setContent {
-            val navController = rememberNavController()
+            navController = rememberNavController()
             RallyNavHost(navController = navController)
         }
 
         // Assertions have to be written outside of the setContent function
-        fail()
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
     }
 }
